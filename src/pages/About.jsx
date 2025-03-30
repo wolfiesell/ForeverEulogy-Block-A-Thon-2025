2f73dc7b47c122c3d243593586855e74659e692b
Wolfgang Sell <wolfiesell@gmail.com>
2025-03-30 03:29:31 -0500

    followed online tutorial: https://pinata.cloud/blog/how-to-ship-a-static-website-to-ipfs-with-pinata/

---
+++ src/pages/About.jsx
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <div className="p-5 items-center">
+      <h1>yo!</h1>
+    </div>
+  )
+}
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,21 @@
+export default function Contact() {
+  return (
+    <div className="p-5 items-center text-center">
+      
+      <p className="text-xl font-semibold">Wolfgang Sell</p>
+      <p className="text-md">Director of Education, KU Blockchain Institute</p>
+      
+      <div className="mt-4">
+        <a href="https://linkedin.com/in/wolfgangsell" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">
+          LinkedIn: linkedin.com/in/wolfgangsell
+        </a>
+      </div>
+      
+      <div className="mt-2">
+        <a href="https://github.com/wolfiesell" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">
+          GitHub: github.com/wolfiesell
+        </a>
+      </div>
+    </div>
+  );
+}
